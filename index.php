580a4221c22065d706eed941bb3ef1c2aa6c9a9b
MikaMr <tovoherisoamikhael@gmail.com>
2023-09-19 14:30:06 +0300

    Mon premier commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+/**chaine de caractère
+$nom= "rakoto";
+//integer
+$age = 25;
+//boolean
+$isAlive =true; 
+//float
+$height =  1.20;
+// language go,scala, python, rust, ruby, java , c
+
+$nom;
+$nom = "Bobo";
+
+//echo $nom;
+
+///ville, adressePost, telephone (nombre),isCapital (bolean)
+
+$ville = "Ambatoroka";
+$adressePost = 101;
+$telephone = 323223362;
+$iscapital = "Antananarivo";
+
+$nom;
+echo $ville = "Ambatoroka";$adressePost=" ";
+*/
+
+/**
+echo "<br>";
+$age = 18;
+if ($age == 18) { //egale a
+echo "ampy taona";
+}
+
+echo "<br>;
+if ($age != 17) {
+    echo "Ampy taona indray e";
+}
+
+//Devoir : tableau boolean 1, 0, true, false
+echo "<br>";
+
+*/
+$age = 18;
+
+if ($age <= 18); {
+    echo "izy";
+}
+
+if ($age >=18);  {
+    echo " tsy izy";
+}
+//**
+
+/**
+echo 18-18;
+echo "<br>";
+echo 18 % 2; // 0
+echo "<br>";
+echo 15 % 2; // 0
+echo "<br>";
+
+echo "<br>";
+echo 9 % 2; // 1
+
+//**
+
+echo 18 == 18;
+
+ if (18 == 18)
+ $nom = "Henintsoa"
+if("babako"==$nom)
+    echo
+//**
+
+*/
+//table boolean appliquer anaty if else + git hub
+// <=> 0, 1, -1
+echo "<br>";
+$note = 13;
+if($note <= 12 || $note >= 15){
+    echo "bien";
+}else{
+    echo "tsy ampy";
+}
